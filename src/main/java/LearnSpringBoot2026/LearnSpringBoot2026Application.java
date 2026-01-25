c7dbb7ba5a2f63e17948a3512dcecbaae77e17b1
Janishar Akhtar <janisharakhtar277@gmail.com>
2026-01-25 22:15:28 +0530

    Spring Boot application started

---
+++ src/main/java/LearnSpringBoot2026/LearnSpringBoot2026Application.java
@@ -0,0 +1,12 @@
+package LearnSpringBoot2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot2026Application.class, args);
+	}
+}
---
+++ src/test/java/LearnSpringBoot2026/LearnSpringBoot2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package LearnSpringBoot2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
